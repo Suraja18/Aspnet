47e4743829ca9234cf22b446270407fcc82c24f5
Suraj Adhikari <sssadhikari222@gmail.com>
2023-06-25 07:43:29 +0545

    4th class

---
+++ AspProject/WriteNewXML.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WriteNewXML.aspx.cs" Inherits="AspProject.WriteNewXML" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="CreateXml" runat="server" Text="Create a new Xml Document" OnClick="CreateXml_Click" />
+        </div>
+        <div>
+            <label>Product</label>
+        </div>
+        <div>
+            <label>ID:</label>
+            <asp:TextBox ID="RegNum" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <label>Product Name:</label>
+            <asp:TextBox ID="PName" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <label>Product Price:</label>
+            <asp:TextBox ID="PPrice" runat="server"></asp:TextBox>
+        </div>
+        <asp:Button ID="Insert" runat="server" Text="Insert" OnClick="Insert_Click" />
+    </form>
+</body>
+</html>
---
+++ AspProject/WriteNewXML.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspProject
+{
+    public partial class WriteNewXML : System.Web.UI.Page
+    {
+        protected void CreateXml_Click(object sender, EventArgs e)
+        {
+            //Create a new , empty document
+            System.Xml.XmlDocument doc = new System.Xml.XmlDocument();
+            System.Xml.XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(docNode);
+
+            //Create and insert a new element
+            System.Xml.XmlNode productsNode = doc.CreateElement("products");
+            doc.AppendChild(productsNode);
+
+            //Create a nested element (with an attribute)
+            System.Xml.XmlNode productNode = doc.CreateElement("product");
+            System.Xml.XmlAttribute productAttribute = doc.CreateAttribute("id");
+            productAttribute.Value = "1001";
+            productNode.Attributes.Append(productAttribute);
+            productsNode.AppendChild(productNode);
+
+            //Create and add the subelementd for this product node (with contained text data)
+            System.Xml.XmlNode nameNode = doc.CreateElement("productName");
+            nameNode.AppendChild(doc.CreateTextNode("3 in 1 Coffee"));
+            productNode.AppendChild(nameNode);
+            System.Xml.XmlNode priceNode = doc.CreateElement("productPrice");
+            priceNode.AppendChild(doc.CreateTextNode("6.99"));
+            productNode.AppendChild(priceNode);
+
+            //Save the document
+            doc.Save(Server.MapPath("product.xml"));
+
+        }
+
+        protected void Insert_Click(object sender, EventArgs e)
+        {
+            //Load XML document
+            System.Xml.XmlDocument doc = new System.Xml.XmlDocument();
+            doc.Load(Server.MapPath("product.xml"));
+
+            //Create a nested element (with an attribute).
+            System.Xml.XmlNode productNode = doc.CreateElement("product");
+            System.Xml.XmlAttribute productAttributes = doc.CreateAttribute("id");
+            productAttributes.Value = this.RegNum.Text;
+            productNode.Attributes.Append(productAttributes);
+            doc.DocumentElement.AppendChild(productNode);
+
+            //Create and add the subelements for this product node (with contained text data).
+            System.Xml.XmlNode nameNode = doc.CreateElement("productName");
+            nameNode.AppendChild(doc.CreateTextNode(this.PName.Text));
+            productNode.AppendChild(nameNode);
+
+            //Create and add the subelements for this product node (with contained text data).
+            System.Xml.XmlNode priceNode = doc.CreateElement("productPrice");
+            priceNode.AppendChild(doc.CreateTextNode(this.PPrice.Text));
+            productNode.AppendChild(priceNode);
+
+            //Save Document
+            doc.Save(Server.MapPath("product.xml"));
+        }
+    }
+}
